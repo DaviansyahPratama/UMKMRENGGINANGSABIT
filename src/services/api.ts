ea08b94c5ead31b9a0d022a9b99a94386925f130
dapi <m.noer24ti@mahasiswa.pcr.ac.id>
2026-06-11 13:51:37 +0700

    coba deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://rengginangsabit.infinityfreeapp.com/api",
 });
 
 export const buildProductImageUrl = (imagePath?: string) => {
